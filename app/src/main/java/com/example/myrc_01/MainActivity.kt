e968de8ae2fc8bf2f08bb7f390081342d95a307a
Hyewon Seo <hyewon9707@gmail.com>
2023-01-28 09:52:51 +0900

    xml layout study(yanolja clone)

---
+++ app/src/main/java/com/example/myrc_01/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myrc_01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+//    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
